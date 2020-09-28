c1673e0803936c3ca41ea847f98d0758e86565c1
zhaohailong <zhaohailong@digisky.com>
2020-09-28 12:06:13 +0800

    add

--- epoll_server.cc
+++ epoll_server.cc
@@ -1,22 +1,45 @@
 #include "stdio.h"
 #include "client_sock.h"
 #include "server.h"
+#include "signal.h"
 
-int main()
-{
-    Server ser;
-    int res = ser.init_ae();
+bool is_running = true;
+static void ctrl_handler(int sig){
+    is_running = false;
+}
+
+static void sock_thread_handler(void* ser){
+    if (NULL == ser)
+    {
+        printf("thread error");
+        return;
+    }
+    
+    int res = ser->init_ae();
     if (res < 0)
     {
         exit(-1);
     }
     
-    res = ser.create_server_sock();
+    res = ser->create_server_sock();
     if (res < 0)
     {
         printf("create error\n");
         return res;
     }
 
-    ser.ae_poll();
+    ser->ae_poll();
+}
+int main()
+{
+    signal(SIGINT, ctrl_handler);
+    Server* pser = new Server;
+    pthread_t t;
+    pthread_create(&t, NULL, sock_thread_handler, (void*)pser);
+    while (is_running)
+    {
+        sleep(1);
+    }
+    pser->set_running_flag(false);
+    t.join();
 }
